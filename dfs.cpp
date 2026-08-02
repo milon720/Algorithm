bde27df2aa2effd3e02f661c50fbde5b49938330
Milon Hosen <s2410476141@ru.ac.bd>
2026-08-03 01:53:16 +0600

    This is DFS Algorithm

---
+++ dfs.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> graph[100];
+vector<bool> vis(100);
+
+void dfs(int u){
+      vis[u]=true;
+      cout<<u<<" ";
+      for(int v : graph[u]){
+         if(!vis[v]) dfs(v);
+      }
+}
+
+int main(){
+    int n,m;
+    cin>>n>>m;
+    for(int i=0;i<m;i++){
+        int u,v;
+        cin>>u>>v;
+        graph[u].push_back(v);
+        graph[v].push_back(u); //if bidirectional graph
+    }
+    dfs(0); //start form 0 node
+
+}
